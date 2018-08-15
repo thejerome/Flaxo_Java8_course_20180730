a96b33c0022286baa543d8e627a4c7b629dfb622
MalyshevI <r_alliance@mail.ru>
2018-08-15 16:41:11 +0300

    Task 2 done.

--- src/test/java/a_lambda/exercise/ArrowNotationExercise.java
+++ src/test/java/a_lambda/exercise/ArrowNotationExercise.java
@@ -4,6 +4,7 @@ package a_lambda.exercise;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.function.Function;
 
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,7 @@ public class ArrowNotationExercise {
     @Test
     public void getAge() {
         // Person -> Integer
-        final Function<Person, Integer> getAge = null; // TODO
+        final Function<Person, Integer> getAge = Person::getAge; // TODO
 
         assertEquals(Integer.valueOf(33), getAge.apply(new Person("", "", 33)));
     }
@@ -25,8 +26,8 @@ public class ArrowNotationExercise {
         // TODO use BiPredicate
         // compareAges: (Person, Person) -> boolean
 
-        throw new UnsupportedOperationException("Not implemented");
-        //assertEquals(true, compareAges.test(new Person("a", "b", 22), new Person("c", "d", 22)));
+        BiPredicate<Person, Person> compareAges = ((person1, person2) -> person1.getAge() == person2.getAge());
+        assertEquals(true, compareAges.test(new Person("a", "b", 22), new Person("c", "d", 22)));
     }
 
     // TODO
@@ -40,11 +41,15 @@ public class ArrowNotationExercise {
     public void getAgeOfPersonWithTheLongestFullName() {
         // Person -> String
         // TODO use getFullName
-        final Function<Person, String> getFullName = null;
+        final Function<Person, String> getFullName = person -> (person.getFirstName() + person.getLastName());
 
         // (Person, Person) -> Integer
         // TODO use ageOfPersonWithTheLongestFullName(getFullName)
-        final BiFunction<Person, Person, Integer> ageOfPersonWithTheLongestFullName = null;
+        final BiFunction<Person, Person, Integer> ageOfPersonWithTheLongestFullName = ((person1, person2) -> {
+            String fullNamePerson1 = person1.getFirstName() + person1.getLastName();
+            String fullNamePerson2 = person2.getFirstName() + person2.getLastName();
+            return fullNamePerson1.length() > fullNamePerson2.length() ? person1.getAge() : person2.getAge();
+        });
 
         assertEquals(
                 Integer.valueOf(1),
