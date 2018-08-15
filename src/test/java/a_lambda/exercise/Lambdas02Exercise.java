921a34a6cf977d056d2870ba7eefe8a1d455fe6a
Sergei Stafeev <Sergei_Stafeev@epam.com>
2018-08-15 12:30:34 +0300

    task-1: style has fixed

--- src/test/java/a_lambda/exercise/Lambdas02Exercise.java
+++ src/test/java/a_lambda/exercise/Lambdas02Exercise.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
 import org.junit.jupiter.api.Test;
 
 import com.google.common.collect.ImmutableList;
@@ -33,7 +35,9 @@ public class Lambdas02Exercise {
     @Test
     public void sortPersonsByAge() {
         final Person[] persons = getPersons();
+
         // TODO use Arrays.sort and lambda
+        Arrays.sort(persons, (o1, o2) -> Integer.compare(o1.getAge(), o2.getAge()));
 
         assertArrayEquals(persons, new Person[]{
                 new Person("name 4", "lastName 7", 21),
@@ -52,7 +56,13 @@ public class Lambdas02Exercise {
     public void findFirstWithAgeGreaterThan30() {
         final List<Person> persons = new ArrayList<>(Arrays.asList(getPersons()));
         Person person = null;
+
         // TODO use FluentIterable and lambda
+        Optional<Person> personOptional = FluentIterable.from(persons).firstMatch(p -> p.getAge() > 30);
+
+        if (personOptional.isPresent()) {
+            person = personOptional.get();
+        }
 
         assertEquals(person, new Person("name 2", "lastName 1", 33));
     }
