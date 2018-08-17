8d81b348fbfce8e84fdceed9407f2e21a22764d4
Andrei_Mironov <Andrei_Mironov@epam.com>
2018-08-17 18:23:10 +0300

    fix codestyle

--- src/test/java/a_lambda/exercise/Mapping.java
+++ src/test/java/a_lambda/exercise/Mapping.java
@@ -7,9 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
