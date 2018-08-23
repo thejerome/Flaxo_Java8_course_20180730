d2c1781c334b3a724d2cc6899e3d3ff78682bc4b
pcsaic <pcsaic@yandex.ru>
2018-08-23 13:16:26 +0300

    Commit task-4

--- src/test/java/b_streams/exercise/StreamsExercise1.java
+++ src/test/java/b_streams/exercise/StreamsExercise1.java
@@ -7,11 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
