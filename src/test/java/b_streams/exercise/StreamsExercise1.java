a4921f078633a8a1b7345d2b9ccbb4df2e9a1d9e
Raya A <r.abuzyarova@gmail.com>
2018-08-23 11:40:14 +0300

    refactoring

--- src/test/java/b_streams/exercise/StreamsExercise1.java
+++ src/test/java/b_streams/exercise/StreamsExercise1.java
@@ -6,11 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.security.cert.CollectionCertStoreParameters;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
