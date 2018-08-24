3e1419b848006b2f3694724008c4fb96de9fed05
Petr Savin <johnnybarden@yandex.ru>
2018-08-24 23:54:33 +0300

    Task 5 in progress.

--- src/test/java/b_streams/exercise/WarAndPeaceExercise.java
+++ src/test/java/b_streams/exercise/WarAndPeaceExercise.java
@@ -3,7 +3,12 @@ package b_streams.exercise;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
@@ -14,12 +19,21 @@ public class WarAndPeaceExercise {
 
     @Test
     public void warAndPeace() throws IOException {
-        Stream.of(
+        Stream<Path> pathStream = Stream.of(
                 Paths.get("src", "test", "resources", "WAP12.txt"),
                 Paths.get("src", "test", "resources", "WAP34.txt"));
 
 
-        String result = null;
+        String result = pathStream
+                .flatMap(path -> {
+                    try {
+                        return Files.lines(path, Charset.forName("windows-1251"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+
         // TODO map lowercased words to its amount in text and concatenate its entries.
         // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
         // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
