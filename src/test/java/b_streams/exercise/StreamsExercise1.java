3c6919897975ffdb9ea3cf20a82628ca3fb943ac
Anton Kulaga <kindet27@gmail.com>
2018-08-24 21:25:30 +0300

    task 4 fix

--- src/test/java/b_streams/exercise/StreamsExercise1.java
+++ src/test/java/b_streams/exercise/StreamsExercise1.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
--- src/test/java/b_streams/exercise/StreamsExercise2.java
+++ src/test/java/b_streams/exercise/StreamsExercise2.java
@@ -3,7 +3,6 @@ package b_streams.exercise;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.*;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
