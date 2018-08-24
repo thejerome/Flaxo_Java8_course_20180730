fc00f8f9af5d7fe2d5b0c8485616906c6d86644d
nikolay <imbirev.nikolay@gmail.com>
2018-08-24 18:40:33 +0300

    task is completed

--- src/test/java/b_streams/exercise/WarAndPeaceExercise.java
+++ src/test/java/b_streams/exercise/WarAndPeaceExercise.java
@@ -3,7 +3,15 @@ package b_streams.exercise;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
@@ -14,18 +22,37 @@ public class WarAndPeaceExercise {
 
     @Test
     public void warAndPeace() throws IOException {
-        Stream.of(
-                Paths.get("src", "test", "resources", "WAP12.txt"),
-                Paths.get("src", "test", "resources", "WAP34.txt"));
 
+        String result = Stream.of(
+            Paths.get("src", "test", "resources", "WAP12.txt"),
+            Paths.get("src", "test", "resources", "WAP34.txt"))
+            .flatMap(WarAndPeaceExercise::readLinesFromPath)
+            .map(String::toLowerCase)
+            .map(string -> string.replaceAll("[^a-zа-я]", " "))
+            .flatMap(string -> Arrays.stream(string.split(" ")))
+            .filter(string -> string.length() >= 4)
+            .collect(Collectors.groupingBy(String::valueOf, TreeMap::new, Collectors.counting()))
+            .entrySet().stream()
+            .filter(entry -> entry.getValue() >= 10)
+            .sorted((firstEntry, secondEntry) -> Long.compare(secondEntry.getValue(), firstEntry.getValue()))
+            .map(entry -> entry.getKey() + " - " + entry.getValue() + "\n")
+            .collect(Collectors.joining())
+            .trim();
 
-        String result = null;
         // TODO map lowercased words to its amount in text and concatenate its entries.
         // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
         // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
         // TODO Also omit any word with lengths less than 4 and frequency less than 10
 
         assertEquals(new WAPResult().result, result);
+    }
+
+    private static Stream<String> readLinesFromPath(Path path) {
+        try {
+            return Files.lines(path, Charset.forName("windows-1251"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
