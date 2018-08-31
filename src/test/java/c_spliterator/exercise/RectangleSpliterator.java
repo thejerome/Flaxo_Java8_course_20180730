439cedb42ef29b943f3d50758a91b33218bd7a75
Raya A <r.abuzyarova@gmail.com>
2018-08-31 23:37:28 +0300

    task 8 is done

--- src/test/java/c_spliterator/exercise/RectangleSpliterator.java
+++ src/test/java/c_spliterator/exercise/RectangleSpliterator.java
@@ -6,26 +6,86 @@ import java.util.function.IntConsumer;
 
 public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
 
+    private final int[][] array;
+    private int firstDimStartInclusive;
+    private int firstDimEndExclusive;
+    private int secondDimStartInclusive;
+    private int secondDimEndExclusive;
+    private int currentPosition;
+
+
     public RectangleSpliterator(int[][] array) {
         //TODO
-        super(0, 0);
+        this(array, 0, array.length, 0, array[0].length, 0);
+    }
+
+    private RectangleSpliterator(int[][] array, int firstDimStartInclusive, int firstDimEndExclusive,
+                                 int secondDimStartInclusive, int secondDimEndExclusive, int currentPosition) {
+        super((firstDimEndExclusive - firstDimStartInclusive) * (secondDimEndExclusive - secondDimStartInclusive),
+                IMMUTABLE
+                        | ORDERED
+                        | SIZED
+                        | SUBSIZED
+                        | NONNULL);
+        this.array = array;
+        this.firstDimStartInclusive = firstDimStartInclusive;
+        this.firstDimEndExclusive = firstDimEndExclusive;
+        this.secondDimStartInclusive = secondDimStartInclusive;
+        this.secondDimEndExclusive = secondDimEndExclusive;
+        this.currentPosition = currentPosition;
     }
 
     @Override
     public RectangleSpliterator trySplit() {
         // TODO
-        throw new UnsupportedOperationException();
+        final int firstDimlength = firstDimEndExclusive - firstDimStartInclusive;
+        final int secondDimlength = secondDimEndExclusive - secondDimStartInclusive;
+        if (firstDimlength < 2 && secondDimlength < 2) {
+            return null;
+        }
+        RectangleSpliterator rectangleSpliterator;
+
+        if (firstDimEndExclusive > secondDimEndExclusive) {
+            int firstMid = firstDimStartInclusive + firstDimlength / 2;
+            int secondMid = secondDimEndExclusive;
+            rectangleSpliterator = new RectangleSpliterator(array, firstDimStartInclusive, firstMid, secondDimStartInclusive, secondMid, currentPosition);
+            firstDimStartInclusive = firstMid;
+        } else {
+            int firstMid = firstDimEndExclusive;
+            int secondMid = secondDimStartInclusive + secondDimlength / 2;
+            rectangleSpliterator = new RectangleSpliterator(array, firstDimStartInclusive, firstMid, secondDimStartInclusive, secondMid, currentPosition);
+            secondDimStartInclusive = secondMid;
+        }
+        return rectangleSpliterator;
     }
 
     @Override
     public long estimateSize() {
         //TODO
-        throw new UnsupportedOperationException();
+        return (firstDimEndExclusive - firstDimStartInclusive - 1) * secondDimEndExclusive + (secondDimEndExclusive - currentPosition);
     }
 
     @Override
     public boolean tryAdvance(IntConsumer action) {
         // TODO
-        throw new UnsupportedOperationException();
+        if (firstDimStartInclusive >= firstDimEndExclusive || secondDimStartInclusive >= secondDimEndExclusive) {
+            return false;
+        }
+        final int value = array[firstDimStartInclusive][secondDimStartInclusive];
+        secondDimStartInclusive += 1;
+        ++currentPosition;
+        action.accept(value);
+        return true;
+    }
+
+    @Override
+    public void forEachRemaining(IntConsumer action) {
+        for (int i = firstDimStartInclusive; i < firstDimEndExclusive; i++) {
+            for (int j = secondDimStartInclusive; j < secondDimEndExclusive; j++) {
+                action.accept(array[i][j]);
+            }
+        }
+        firstDimStartInclusive = firstDimEndExclusive;
+        secondDimStartInclusive = secondDimEndExclusive;
     }
 }
