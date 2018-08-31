0a4b61e9f9899b0c08ede57e4a78a9ef3d3667a5
Andrei_Mironov <Andrei_Mironov@epam.com>
2018-08-31 14:18:30 +0300

    task-9 complete

--- src/test/java/c_spliterators/exercise/ListZipSpliterator.java
+++ src/test/java/c_spliterators/exercise/ListZipSpliterator.java
@@ -1,12 +1,10 @@
 package c_spliterators.exercise;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Spliterator;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 public class ListZipSpliterator<L, R, T> implements Spliterator<T> {
     private final List<L> list1;
@@ -14,52 +12,63 @@ public class ListZipSpliterator<L, R, T> implements Spliterator<T> {
     private final Iterator<L> list1Iterator;
     private final Iterator<R> list2Iterator;
     private final BiFunction<L, R, T> combiner;
+    private int startInclusive;
+    private final int endExclusive;
 
 
     public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
+        this(list1, list2, combiner, 0);
+    }
+
+    private ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner,
+                               int startInclusive) {
         this.list1 = list1;
         this.list2 = list2;
         this.list1Iterator = list1.iterator();
         this.list2Iterator = list2.iterator();
         this.combiner = combiner;
+        this.startInclusive = startInclusive;
+        this.endExclusive = list1.size() > list2.size() ? list2.size() : list1.size();
     }
 
     @Override
     public boolean tryAdvance(Consumer<? super T> action) {
         //TODO
-        if (list1Iterator.hasNext() && list2Iterator.hasNext()) {
-            final L elem1 = list1Iterator.next();
-            final R elem2 = list2Iterator.next();
-            final T combined = combiner.apply(elem1, elem2);
-            action.accept(combined);
-            return true;
+        if (startInclusive >= endExclusive) {
+            return false;
         }
-        return false;
+        final L elem1 = list1.get(startInclusive);
+        final R elem2 = list2.get(startInclusive);
+        final T combined = combiner.apply(elem1, elem2);
+        action.accept(combined);
+        startInclusive += 1;
+        return true;
 //        throw new UnsupportedOperationException();
     }
 
     @Override
     public Spliterator<T> trySplit() {
         //TODO
-        if (list1.size() <= 1 || list2.size() <= 1) {
+        final int length = endExclusive - startInclusive;
+        if (length < 2) {
             return null;
         }
-        final List<L> resultList1 = new ArrayList<>();
-        final List<R> resultList2 = new ArrayList<>();
-        final long workSize = estimateSize() / 2;
 
-        for (int i = 0; i < workSize; i++) {
-            resultList1.add(list1.get(i));
-            resultList2.add(list2.get(i));
-        }
-        return new ListZipSpliterator<L, R, T>(resultList1, resultList2, combiner);
+        final int mid = startInclusive + length / 2;
+
+        final List<L> resultList1 = list1.subList(startInclusive, mid);
+        final List<R> resultList2 = list2.subList(startInclusive, mid);
+
+        startInclusive = mid;
+
+        return new ListZipSpliterator<>(resultList1, resultList2, combiner);
 //        throw new UnsupportedOperationException();
     }
 
     @Override
     public long estimateSize() {
         //TODO
-        return list1.size() > list2.size() ? list2.size() : list1.size();
+        return endExclusive - startInclusive;
 //        throw new UnsupportedOperationException();
     }
 
@@ -70,7 +79,7 @@ public class ListZipSpliterator<L, R, T> implements Spliterator<T> {
                 | SIZED
                 | SUBSIZED
                 | NONNULL
-                | IMMUTABLE;
+                | DISTINCT;
 //        throw new UnsupportedOperationException();
     }
 }
