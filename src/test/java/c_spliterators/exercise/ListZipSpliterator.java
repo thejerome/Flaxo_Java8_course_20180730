17c2c490d431c500dccce065f7891404be08fad2
Andrei_Mironov <Andrei_Mironov@epam.com>
2018-08-31 13:11:24 +0300

    task-9 in progress

--- src/test/java/c_spliterators/exercise/ListZipSpliterator.java
+++ src/test/java/c_spliterators/exercise/ListZipSpliterator.java
@@ -1,36 +1,76 @@
 package c_spliterators.exercise;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Spliterator;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class ListZipSpliterator<L, R, T> implements Spliterator<T> {
-    public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
+    private final List<L> list1;
+    private final List<R> list2;
+    private final Iterator<L> list1Iterator;
+    private final Iterator<R> list2Iterator;
+    private final BiFunction<L, R, T> combiner;
 
+
+    public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
+        this.list1 = list1;
+        this.list2 = list2;
+        this.list1Iterator = list1.iterator();
+        this.list2Iterator = list2.iterator();
+        this.combiner = combiner;
     }
 
     @Override
     public boolean tryAdvance(Consumer<? super T> action) {
         //TODO
-        throw new UnsupportedOperationException();
+        if (list1Iterator.hasNext() && list2Iterator.hasNext()) {
+            final L elem1 = list1Iterator.next();
+            final R elem2 = list2Iterator.next();
+            final T combined = combiner.apply(elem1, elem2);
+            action.accept(combined);
+            return true;
+        }
+        return false;
+//        throw new UnsupportedOperationException();
     }
 
     @Override
     public Spliterator<T> trySplit() {
         //TODO
-        throw new UnsupportedOperationException();
+        if (list1.size() <= 1 || list2.size() <= 1) {
+            return null;
+        }
+        final List<L> resultList1 = new ArrayList<>();
+        final List<R> resultList2 = new ArrayList<>();
+        final long workSize = estimateSize() / 2;
+
+        for (int i = 0; i < workSize; i++) {
+            resultList1.add(list1.get(i));
+            resultList2.add(list2.get(i));
+        }
+        return new ListZipSpliterator<L, R, T>(resultList1, resultList2, combiner);
+//        throw new UnsupportedOperationException();
     }
 
     @Override
     public long estimateSize() {
         //TODO
-        throw new UnsupportedOperationException();
+        return list1.size() > list2.size() ? list2.size() : list1.size();
+//        throw new UnsupportedOperationException();
     }
 
     @Override
     public int characteristics() {
         //TODO
-        throw new UnsupportedOperationException();
+        return IMMUTABLE
+                | SIZED
+                | SUBSIZED
+                | NONNULL
+                | IMMUTABLE;
+//        throw new UnsupportedOperationException();
     }
 }
