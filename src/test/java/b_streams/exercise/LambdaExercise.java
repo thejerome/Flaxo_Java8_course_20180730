53860b3f085523d75da43e2bd9bdfc236f3ba1e1
Anton Kulaga <kindet27@gmail.com>
2018-08-31 13:42:15 +0300

    initial

--- src/test/java/b_streams/exercise/LambdaExercise.java
+++ src/test/java/b_streams/exercise/LambdaExercise.java
@@ -16,19 +16,22 @@ public class LambdaExercise {
     public void supply() {
         final Person person = new Person("John", "Galt", 30);
 
-        final Supplier<Person> getPerson = null; // TODO return person from Supplier
+        final Supplier<Person> getPerson = () -> person; // DONE return person from Supplier
 
         assertEquals(person, getPerson.get());
     }
 
     @Test
     public void function() {
-        final Function<Person, String> getPersonName1 = null; // TODO get the name of person using expression lambda
+        final Function<Person, String> getPersonName1 = p -> p.getFirstName(); // DONE get the name of person using expression lambda
 
-        final Function<Person, String> getPersonName2 = null; // TODO get the name of person using method reference
+        final Function<Person, String> getPersonName2 = Person::getFirstName; // DONE get the name of person using method reference
 
-        // TODO get the name of person and log it to System.out using statement lambda: {}
-        final Function<Person, String> getPersonNameAndLogIt = null;
+        // DONE get the name of person and log it to System.out using statement lambda: {}
+        final Function<Person, String> getPersonNameAndLogIt = p -> {
+            System.out.println(p.getFirstName());
+            return p.getFirstName();
+        };
 
         final Person person = new Person("John", "Galt", 30);
 
@@ -39,19 +42,21 @@ public class LambdaExercise {
 
     @Test
     public void combineFunctions() {
-        final Function<Person, String> getPersonName = null; // TODO get the name of person
+        final Function<Person, String> getPersonName = Person::getFirstName; // DONE get the name of person
 
         assertEquals("John", getPersonName.apply(new Person("John", "Galt", 30)));
 
-        final Function<String, Integer> getStringLength = null; // TODO get string length
+        final Function<String, Integer> getStringLength = String::length; // DONE get string length
 
         assertEquals(Integer.valueOf(3), getStringLength.apply("ABC"));
 
-        // TODO get person name length using getPersonName and getStringLength without andThen
-        final Function<Person, Integer> getPersonNameLength1 = null;
+        // DONE get person name length using getPersonName and getStringLength without andThen
+        final Function<Person, Integer> getPersonNameLength1 = p -> getStringLength.apply(getPersonName.apply(p));
 
-        // TODO get person name length using getPersonName and getStringLength with andThen
-        final Function<Person, Integer> getPersonNameLength2 = null;
+        // DONE get person name length using getPersonName and getStringLength with andThen
+        final Function<Person, Integer> getPersonNameLength2 = p -> getPersonName
+                                                                    .andThen(getStringLength)
+                                                                    .apply(p);
 
         final Person person = new Person("John", "Galt", 30);
 
@@ -69,26 +74,29 @@ public class LambdaExercise {
 
     // ((T -> R), (R -> boolean)) -> (T -> boolean)
     private <T, R> Predicate<T> combine(Function<T, R> f, Predicate<R> p) {
-        // TODO
-        throw new UnsupportedOperationException();
+        return t -> p.test(f.apply(t));
     }
 
     @Test
     public void methodReference() {
         // TODO use only method reverences here.
-        final Person person = createPerson(null); // TODO
+        final Person person = createPerson(Person::new); // TODO
 
         assertEquals(new Person("John", "Galt", 66), person);
 
-        final Function<Person, String> getPersonName = null; // TODO
+        final Function<Person, String> getPersonName = Person::getFirstName; // TODO
 
         assertEquals("John", getPersonName.apply(person));
 
-        final Predicate<String> isJohnString = null; // TODO using method reference check that "John" equals string parameter
+        final Predicate<String> isJohnString = LambdaExercise::isJhon; // TODO using method reference check that "John" equals string parameter
 
         final Predicate<Person> isJohnPerson = combine(getPersonName, isJohnString);
 
         assertEquals(true, isJohnPerson.test(person));
+    }
+
+    private static boolean isJhon(String s){
+        return "John".equals(s);
     }
 
 }
--- src/test/java/b_streams/exercise/StreamsExercise.java
+++ src/test/java/b_streams/exercise/StreamsExercise.java
@@ -2,10 +2,8 @@ package b_streams.exercise;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.junit.jupiter.api.Test;
@@ -20,7 +18,11 @@ public class StreamsExercise {
     public void getAllJobHistoryEntries() {
         final List<Employee> employees = getEmployees();
 
-        final List<JobHistoryEntry> jobHistoryEntries = null; // TODO
+        final List<JobHistoryEntry> jobHistoryEntries = employees
+                .stream()
+                .flatMap(e -> e.getJobHistory().stream())
+                .collect(Collectors.toList());
+                 // TODO
 
         assertEquals(22, jobHistoryEntries.size());
     }
@@ -30,7 +32,13 @@ public class StreamsExercise {
         // sum all durations for all persons
         final List<Employee> employees = getEmployees();
 
-        final int sumDurations = 0; // TODO
+        final int sumDurations = employees
+                .stream()
+                .flatMap(e -> e.getJobHistory().stream())
+                .mapToInt(JobHistoryEntry::getDuration)
+                .sum();
+
+                // TODO
 
         assertEquals(72, sumDurations);
     }
@@ -65,7 +73,12 @@ public class StreamsExercise {
     public void indexPersonsByEmployer1() {
         final List<Employee> employees = getEmployees();
 
-        final Map<String, List<PersonEmployer>> index = null; // TODO
+        final Map<String, List<PersonEmployer>> index = employees
+                .stream()
+                .flatMap(e -> e.getJobHistory().stream().map(jhe -> new PersonEmployer(e.getPerson(), jhe.getEmployer())))
+                .collect(Collectors.groupingBy(PersonEmployer::getEmployer));
+
+                 // TODO
 
         assertEquals(11, index.get("epam").size());
     }
@@ -74,7 +87,10 @@ public class StreamsExercise {
     public void indexPersonsByEmployer2() {
         final List<Employee> employees = getEmployees();
 
-        final Map<String, List<Person>> index = null; // TODO
+        final Map<String, List<Person>> index = employees
+                .stream()
+                .flatMap(e -> e.getJobHistory().stream().map(jhe -> new PersonEmployer(e.getPerson(), jhe.getEmployer())))
+                .collect(Collectors.groupingBy(PersonEmployer::getEmployer, Collectors.mapping(PersonEmployer::getPerson, Collectors.toList()))); // TODO
 
         assertEquals(11, index.get("epam").size());
     }
@@ -106,8 +122,10 @@ public class StreamsExercise {
     }
 
     private PersonDuration sumAllPersonDurations(Employee e) {
-        // TODO
-        throw new UnsupportedOperationException();
+        return new PersonDuration(e.getPerson(), e.getJobHistory()
+                .stream()
+                .mapToInt(JobHistoryEntry::getDuration)
+                .sum());
     }
 
     @Test
@@ -115,7 +133,11 @@ public class StreamsExercise {
         // sum all durations for each person
         final List<Employee> employees = getEmployees();
 
-        final Map<Person, Integer> personDuration = null; // TODO use sumAllPersonDurations
+        final Map<Person, Integer> personDuration = employees
+                .stream()
+                .map(this::sumAllPersonDurations)
+                .collect(Collectors.toMap(PersonDuration::getPerson, PersonDuration::getDuration));
+                // TODO use sumAllPersonDurations
 
         assertEquals(Integer.valueOf(8), personDuration.get(new Person("John", "Doe", 24)));
     }
@@ -139,15 +161,20 @@ public class StreamsExercise {
     }
 
     private static PersonPositionIndex getPersonPositionIndex(Employee e) {
-        // TODO
-        throw new UnsupportedOperationException();
+        return new PersonPositionIndex(e.getPerson(), e.getJobHistory()
+                .stream()
+                .collect(Collectors.toMap(JobHistoryEntry::getPosition, JobHistoryEntry::getDuration, Integer::sum)));
     }
 
     @Test
     public void getSumDurationsForPersonByPosition() {
         final List<Employee> employees = getEmployees();
 
-        final List<PersonPositionIndex> personIndexes = null; // TODO use getPersonPositionIndex
+        final List<PersonPositionIndex> personIndexes = employees
+                .stream()
+                .map(StreamsExercise::getPersonPositionIndex)
+                .collect(Collectors.toList());
+                // TODO use getPersonPositionIndex
 
         assertEquals(1, personIndexes.get(3).getDurationByPositionIndex().size());
     }
@@ -180,7 +207,22 @@ public class StreamsExercise {
     public void getDurationsForEachPersonByPosition() {
         final List<Employee> employees = getEmployees();
 
-        final List<PersonPositionDuration> personPositionDurations =  null; // TODO
+        final List<PersonPositionDuration> personPositionDurations =
+                employees.stream()
+                .flatMap(e -> e.getJobHistory()
+                        .stream()
+                        .collect(Collectors.groupingBy(JobHistoryEntry::getPosition))
+                        .entrySet()
+                        .stream()
+                        .map(sljhe -> new PersonPositionDuration(
+                                e.getPerson(),
+                                sljhe.getKey(),
+                                sljhe.getValue().stream()
+                                        .mapToInt(JobHistoryEntry::getDuration)
+                                        .sum())))
+                .collect(Collectors.toList())
+                        ;
+    // TODO
 
 
         assertEquals(17, personPositionDurations.size());
@@ -191,7 +233,24 @@ public class StreamsExercise {
         // Get person with max duration on given position
         final List<Employee> employees = getEmployees();
 
-        final Map<String, PersonPositionDuration> coolestPersonByPosition = null;// TODO
+        final Map<String, PersonPositionDuration> coolestPersonByPosition = employees.stream()
+                .flatMap(e -> e.getJobHistory()
+                        .stream()
+                        .collect(Collectors.groupingBy(JobHistoryEntry::getPosition))
+                        .entrySet()
+                        .stream()
+                        .map(sljhe -> new PersonPositionDuration(
+                                e.getPerson(),
+                                sljhe.getKey(),
+                                sljhe.getValue().stream()
+                                        .mapToInt(JobHistoryEntry::getDuration)
+                                        .sum())))
+                .collect(Collectors.toList())
+                .stream()
+                .collect(Collectors.groupingBy(PersonPositionDuration::getPosition, Collectors.maxBy(Comparator.comparing(PersonPositionDuration::getDuration))));
+
+                ;
+
 
 
         assertEquals(new Person("John", "White", 22), coolestPersonByPosition.get("QA").getPerson());
