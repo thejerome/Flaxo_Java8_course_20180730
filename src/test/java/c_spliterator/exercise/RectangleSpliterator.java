89fb98e23f65c5d7615bfbe692be537c01dda3d5
Petr Savin <johnnybarden@yandex.ru>
2018-09-02 02:46:01 +0300

    Merge remote-tracking branch 'upstream/task-8' into task-8

--- src/test/java/c_spliterator/exercise/RectangleSpliterator.java
+++ src/test/java/c_spliterator/exercise/RectangleSpliterator.java
@@ -6,15 +6,27 @@ import java.util.function.IntConsumer;
 
 public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
 
-    private int RowStartInclusive;
-    private int RowEndExclusive;
-    private int ColumnStartInclusive;
-    private int ColumnEndExclusive;
+    private final int[][] array;
+    private int rowStartInclusive;
+    private int rowEndExclusive;
+    private int columnStartInclusive;
+    private int columnEndExclusive;
     private int cursor;
 
     public RectangleSpliterator(int[][] array) {
         //TODO
-        super(0, 0);
+        this(array, 0, array.length, 0, array[0].length, 0);
+    }
+    private RectangleSpliterator(int[][] array, int rowStartInclusive, int rowEndExclusive,
+        int columnStartInclusive, int columnEndExclusive, int cursor) {
+        super((rowEndExclusive - rowStartInclusive) * (columnEndExclusive - columnStartInclusive),
+            IMMUTABLE | ORDERED | SIZED | SUBSIZED | NONNULL);
+        this.array = array;
+        this.rowStartInclusive = rowStartInclusive;
+        this.rowEndExclusive = rowEndExclusive;
+        this.columnStartInclusive = columnStartInclusive;
+        this.columnEndExclusive = columnEndExclusive;
+        this.cursor = cursor;
     }
 
     @Override
