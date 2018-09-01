e3e608568a319b1d9f0b4b89eb750988e6709cb3
Petr Savin <johnnybarden@yandex.ru>
2018-09-02 02:08:14 +0300

    Merge remote-tracking branch 'upstream/task-7' into task-7

--- src/test/java/b_streams/exercise/LambdaExercise.java
+++ src/test/java/b_streams/exercise/LambdaExercise.java
@@ -16,19 +16,23 @@ public class LambdaExercise {
     public void supply() {
         final Person person = new Person("John", "Galt", 30);
 
-        final Supplier<Person> getPerson = null; // TODO return person from Supplier
+        final Supplier<Person> getPerson = () -> person; // TODO return person from Supplier
 
         assertEquals(person, getPerson.get());
     }
 
     @Test
     public void function() {
-        final Function<Person, String> getPersonName1 = null; // TODO get the name of person using expression lambda
+        final Function<Person, String> getPersonName1 = person1 -> person1.getFirstName(); // TODO get the name of person using expression lambda
 
-        final Function<Person, String> getPersonName2 = null; // TODO get the name of person using method reference
+        final Function<Person, String> getPersonName2 = Person::getFirstName; // TODO get the name of person using method reference
 
         // TODO get the name of person and log it to System.out using statement lambda: {}
-        final Function<Person, String> getPersonNameAndLogIt = null;
+        final Function<Person, String> getPersonNameAndLogIt = p -> {
+            String personName = p.getFirstName();
+            System.out.println(personName);
+            return personName;
+        };
 
         final Person person = new Person("John", "Galt", 30);
 
@@ -39,19 +43,19 @@ public class LambdaExercise {
 
     @Test
     public void combineFunctions() {
-        final Function<Person, String> getPersonName = null; // TODO get the name of person
+        final Function<Person, String> getPersonName = Person::getFirstName; // TODO get the name of person
 
         assertEquals("John", getPersonName.apply(new Person("John", "Galt", 30)));
 
-        final Function<String, Integer> getStringLength = null; // TODO get string length
+        final Function<String, Integer> getStringLength = String::length; // TODO get string length
 
         assertEquals(Integer.valueOf(3), getStringLength.apply("ABC"));
 
         // TODO get person name length using getPersonName and getStringLength without andThen
-        final Function<Person, Integer> getPersonNameLength1 = null;
+        final Function<Person, Integer> getPersonNameLength1 = person -> getStringLength.apply(getPersonName.apply(person));
 
         // TODO get person name length using getPersonName and getStringLength with andThen
-        final Function<Person, Integer> getPersonNameLength2 = null;
+        final Function<Person, Integer> getPersonNameLength2 = getPersonName.andThen(getStringLength);
 
         final Person person = new Person("John", "Galt", 30);
 
@@ -70,13 +74,13 @@ public class LambdaExercise {
     // ((T -> R), (R -> boolean)) -> (T -> boolean)
     private <T, R> Predicate<T> combine(Function<T, R> f, Predicate<R> p) {
         // TODO
-        throw new UnsupportedOperationException();
+        return t -> p.test(f.apply(t));
     }
 
     @Test
     public void methodReference() {
         // TODO use only method reverences here.
-        final Person person = createPerson(null); // TODO
+        final Person person = createPerson(Person::new); // TODO
 
         assertEquals(new Person("John", "Galt", 66), person);
 
