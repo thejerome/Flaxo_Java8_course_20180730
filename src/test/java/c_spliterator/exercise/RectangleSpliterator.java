36e65909bd1e19c1b5eadd6fbc63dd239c54849b
akalji <akalji@ya.ru>
2018-09-02 16:36:32 +0300

    Task 8 is done

--- src/test/java/c_spliterator/exercise/RectangleSpliterator.java
+++ src/test/java/c_spliterator/exercise/RectangleSpliterator.java
@@ -1,31 +1,89 @@
 package c_spliterator.exercise;
 
-
+import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.IntConsumer;
 
 public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
 
+    private final int[][] array;
+
+    private int startRow;
+    private int endRow;
+    private int startColumn;
+    private int endColumn;
+    private int currentColumn;
+
     public RectangleSpliterator(int[][] array) {
-        //TODO
-        super(0, 0);
+        this(array, 0, array.length, 0, 0, array[0].length);
+    }
+
+    private RectangleSpliterator(int[][] array, int startRow, int endRow, int startColumn,
+                                 int currentColumn, int endColumn) {
+
+        super((endRow - startRow) * (endColumn - startColumn),
+                Spliterator.IMMUTABLE
+                        | Spliterator.NONNULL
+                        | Spliterator.ORDERED
+                        | Spliterator.SIZED
+                        | Spliterator.SUBSIZED
+        );
+
+        this.array = array;
+        this.startRow = startRow;
+        this.endRow = endRow;
+        this.startColumn = startColumn;
+        this.currentColumn = currentColumn;
+        this.endColumn = endColumn;
     }
 
     @Override
     public RectangleSpliterator trySplit() {
-        // TODO
-        throw new UnsupportedOperationException();
+        final RectangleSpliterator rectangleSpliterator;
+        int rows = endRow - startRow;
+        int columns = endColumn - startColumn;
+
+        if (rows < 2 && columns < 2) {
+            return null;
+        }
+
+        int middle;
+        if (rows >= columns) {
+            middle = startRow + rows / 2;
+            rectangleSpliterator = new RectangleSpliterator(array, startRow, middle, startColumn,
+                    currentColumn, endColumn);
+            startRow = middle;
+            currentColumn = startColumn;
+        } else {
+            middle = startColumn + columns / 2;
+            if (currentColumn >= middle) {
+                rectangleSpliterator = new RectangleSpliterator(array, ++startRow, endRow, startColumn, startColumn, middle);
+            } else {
+                rectangleSpliterator = new RectangleSpliterator(
+                        array, startRow, endRow, startColumn, currentColumn, middle);
+                currentColumn = middle;
+            }
+            startColumn = middle;
+        }
+        return rectangleSpliterator;
     }
 
     @Override
     public long estimateSize() {
-        //TODO
-        throw new UnsupportedOperationException();
+        return (endRow - startRow - 1) * endColumn + (endColumn - currentColumn);
     }
 
     @Override
     public boolean tryAdvance(IntConsumer action) {
-        // TODO
-        throw new UnsupportedOperationException();
+        if (startRow < endRow) {
+            action.accept(array[startRow][currentColumn]);
+            if (++currentColumn == endColumn) {
+                startRow++;
+                currentColumn = startColumn;
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 }
