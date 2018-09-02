a0b43f086a9eb777c01e92e36f3256c86bf679f1
lostforstupid <lostforstupid@gmail.com>
2018-09-02 19:03:20 +0300

    task 9 is done

--- src/test/java/c_spliterators/exercise/ListZipSpliterator.java
+++ src/test/java/c_spliterators/exercise/ListZipSpliterator.java
@@ -6,31 +6,62 @@ import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 public class ListZipSpliterator<L, R, T> implements Spliterator<T> {
+
+    private BiFunction<L, R, T> combiner;
+    private int start;
+    private int end;
+    private List<L> list1;
+    private List<R> list2;
+
     public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
 
+        this.combiner = combiner;
+        start = 0;
+
+        if (list1.size() > list2.size()) {
+            this.list1 = list1.subList(0, list2.size());
+            this.list2 = list2;
+        } else {
+            this.list2 = list2.subList(0, list1.size());
+            this.list1 = list1;
+        }
+
+        end = this.list1.size();
     }
 
     @Override
     public boolean tryAdvance(Consumer<? super T> action) {
-        //TODO
-        throw new UnsupportedOperationException();
+        if (start < end) {
+            action.accept(combiner.apply(list1.get(start), list2.get(start)));
+            start++;
+            return true;
+        }
+
+        return false;
     }
 
     @Override
     public Spliterator<T> trySplit() {
-        //TODO
-        throw new UnsupportedOperationException();
+        if (estimateSize() < 2) {
+            return null;
+        }
+
+        int middle = start + (int) ((estimateSize() / 2));
+
+        ListZipSpliterator<L, R, T> spliterator = new ListZipSpliterator<>(list1.subList(0, middle), list2.subList(0, middle), combiner);
+
+        start = middle;
+
+        return spliterator;
     }
 
     @Override
     public long estimateSize() {
-        //TODO
-        throw new UnsupportedOperationException();
+        return end - start;
     }
 
     @Override
     public int characteristics() {
-        //TODO
-        throw new UnsupportedOperationException();
+        return IMMUTABLE | ORDERED | SIZED | SUBSIZED | NONNULL;
     }
 }
