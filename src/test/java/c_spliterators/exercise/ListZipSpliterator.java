87f3c1061143921bd4d193af185372a9dce682f1
Petr Savin <johnnybarden@yandex.ru>
2018-09-02 15:51:41 +0300

    Task 9 is in progress.

--- src/test/java/c_spliterators/exercise/ListZipSpliterator.java
+++ src/test/java/c_spliterators/exercise/ListZipSpliterator.java
@@ -6,8 +6,21 @@ import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 public class ListZipSpliterator<L, R, T> implements Spliterator<T> {
-    public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
 
+    private final List<L> list1;
+    private final List<R> list2;
+    private final BiFunction<L, R, T> combiner;
+    private int current;
+
+    public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
+        this(list1, list2, combiner, 0);
+    }
+
+    private ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner, int current) {
+        this.list1 = list1;
+        this.list2 = list2;
+        this.combiner = combiner;
+        this.current = current;
     }
 
     @Override
@@ -31,6 +44,10 @@ public class ListZipSpliterator<L, R, T> implements Spliterator<T> {
     @Override
     public int characteristics() {
         //TODO
-        throw new UnsupportedOperationException();
+        return IMMUTABLE
+            | NONNULL
+            | ORDERED
+            | SIZED
+            | SUBSIZED;
     }
 }
