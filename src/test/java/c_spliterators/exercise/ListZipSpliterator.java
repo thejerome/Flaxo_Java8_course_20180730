e82bcf4272a0bf958661499d17b1132d935828cb
venedikttsulenev <venedikttsulenev@list.ru>
2018-09-02 22:56:19 +0300

    Task-9 completed

--- src/test/java/c_spliterators/exercise/ListZipSpliterator.java
+++ src/test/java/c_spliterators/exercise/ListZipSpliterator.java
@@ -6,31 +6,59 @@ import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 public class ListZipSpliterator<L, R, T> implements Spliterator<T> {
-    public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
+    private final List<L> list1;
+    private final List<R> list2;
+    private final BiFunction<L, R, T> combiner;
+    private int leftBound;
+    private final int rightBound;
+    private int index;
 
+    private ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner, int leftBound, int rightBound, int index) {
+        this.list1 = list1;
+        this.list2 = list2;
+        this.combiner = combiner;
+        this.leftBound = leftBound;
+        this.rightBound = rightBound;
+        this.index = index;
+    }
+
+    public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
+        this(list1, list2, combiner, 0, Math.min(list1.size(), list2.size()), 0);
     }
 
     @Override
     public boolean tryAdvance(Consumer<? super T> action) {
         //TODO
-        throw new UnsupportedOperationException();
+        if (index < rightBound) {
+            action.accept(combiner.apply(list1.get(index), list2.get(index)));
+            ++index;
+            return true;
+        }
+        return false;
     }
 
     @Override
     public Spliterator<T> trySplit() {
         //TODO
-        throw new UnsupportedOperationException();
+        final int width = rightBound - leftBound;
+        if (width < 2)
+            return null;
+        final int newSpliteratorRightBound = leftBound + width / 2;
+        Spliterator<T> newSpliterator = new ListZipSpliterator<>(list1, list2, combiner, 0, newSpliteratorRightBound, index);
+        this.leftBound = newSpliteratorRightBound;
+        this.index = leftBound;
+        return newSpliterator;
     }
 
     @Override
     public long estimateSize() {
         //TODO
-        throw new UnsupportedOperationException();
+        return rightBound - leftBound;
     }
 
     @Override
     public int characteristics() {
         //TODO
-        throw new UnsupportedOperationException();
+        return IMMUTABLE | SIZED | SUBSIZED;
     }
 }
